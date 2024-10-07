9c8e421fb84d347d2931fb8daa6efe9acad2a385
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-10-07 18:34:52 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ const { adminAuth } = require("./middlewares/auth");
 const app = express();
 app.use("/admin", adminAuth);
 
-app.post("/user", (req, res) => {
-  res.send("User api called");
+app.get("/user", (req, res) => {
+  throw new error("abcd");
+  // res.send("User api called");
 });
 app.get("/admin/dashboard", (req, res) => {
   res.send("You are on dashboard");
 });
 app.get("/admin/portfolio", (req, res) => {
   res.send("You are in PortFolio");
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong!!!");
+  }
 });
 
 app.listen(7777, () => {
