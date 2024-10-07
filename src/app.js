b4031082c1d45ee6abae7fcbaf9286cbca8bcd8a
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-10-07 17:23:21 +0530

    Handling_other_type_of_request

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/myname", (req, res) => {
-  res.send("my name is Dilip sarvaiya");
+app.post("/user", (req, res) => {
+  console.log(req.params);
+  res.send("data saved to the database");
 });
-
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
+app.get("/user/:userId/:secondId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "dilip", lastName: "Sarvaiya" });
 });
+app.delete("/user", (req, res) => {
+  res.send("data deleted successfully");
+});
+// app.use("/", (req, res) => {
+//   res.send("Hello from dashboard");
+// });
 
 app.listen(7777, () => {
   console.log("server running on port 7777");
