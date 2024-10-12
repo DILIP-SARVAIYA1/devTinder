0d851de4d452c3d97ac58598fc3aae192e0cbe73
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-10-12 16:20:45 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -3,19 +3,42 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   //create new instance of user model
-  const user = new User({
-    firstName: "Dilip",
-    lastName: "Sarvaiya",
-    emailId: "dilipsarvaiya992@gmail.com",
-    password: "Dilip@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User data successfully added 😊");
   } catch (err) {
     res.status(400).send("data not successfully saved!!! 😭");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const emailId = req.body.emailId;
+  const users = await User.find({ emailId: emailId });
+  try {
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  // const emailId = req.body.emailId;
+  const users = await User.find({});
+  try {
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
   }
 });
 
