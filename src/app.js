61d5e554ce7ef568c54cd3805950af10888c26e9
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-10-12 19:08:54 +0530

    update and delete data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
+
+//push data to database
 app.post("/signup", async (req, res) => {
   //create new instance of user model
   const user = new User(req.body);
@@ -14,7 +16,7 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("data not successfully saved!!! 😭");
   }
 });
-
+//get data from the database
 app.get("/user", async (req, res) => {
   const emailId = req.body.emailId;
   const users = await User.find({ emailId: emailId });
@@ -28,6 +30,7 @@ app.get("/user", async (req, res) => {
     res.status(400).send("Something went wrong");
   }
 });
+//find data from the database
 app.get("/feed", async (req, res) => {
   // const emailId = req.body.emailId;
   const users = await User.find({});
@@ -36,6 +39,33 @@ app.get("/feed", async (req, res) => {
       res.status(404).send("User not found");
     } else {
       res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+//delete data from the database
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  const data = await User.findByIdAndDelete(id);
+  console.log(data);
+
+  try {
+    res.send("User delete successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update the data from the database
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const user = await User.findByIdAndUpdate(data.id, data);
+  try {
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User updated successfully");
     }
   } catch (error) {
     res.status(400).send("Something went wrong");
